586418ffe6b2867589656985287d8dff1f0f73f2
TheGuayeah <irofer96@gmail.com>
2024-10-04 10:27:14 +0200

    🪳🧱 Fixed Obstacles not blocking Unit Actions

--- Assets/_Project/Scripts/Runtime/MouseWorld.cs
+++ Assets/_Project/Scripts/Runtime/MouseWorld.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class MouseWorld : Singleton<MouseWorld>
 {
    [SerializeField]
-   private LayerMask mousePlaneLayerMask;
+   private LayerMask unignoredPositions;
+
+   //private void Update()
+   //{
+   //   transform.position = GetPosition();
+   //}
 
    public static Vector3 GetPosition()
    {
       Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-      Physics.Raycast(ray, out RaycastHit hitInfo, float.MaxValue, Instance.mousePlaneLayerMask);
+      Physics.Raycast(ray, out RaycastHit hitInfo, float.MaxValue, Instance.unignoredPositions);
       return hitInfo.point;
    }
 }
